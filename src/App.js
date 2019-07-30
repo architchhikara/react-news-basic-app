7b3da7fd014cce4943fd0011515ffb389d82041b
Archit Chhikara <architchhikara@Archits-MacBook-Air.local>
2019-07-30 12:18:06 +0530

    Adding the Dismiss Functionality

--- src/App.js
+++ src/App.js
@@ -23,26 +23,45 @@ const list = [
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      list: list
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    function isNotID(item) {
+      return item.objectID !== id;
+    }
+    const updatedList = this.state.list.filter(isNotID);
+    // or const updatedList = this.state.list.filter(item => item.objectID !== id);
+
+    this.setState({ list:updatedList })
   }
 
   render() {
-    var helloWorld = "Welcome to the Road to learn React";
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        {list.map(function(item) {
-          return (
-            <div key={item.objectID}>
-              {" "}
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
